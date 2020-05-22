e6de03fb3fd44f9ea334a910ed5d63542a25df80
Mark McDonald <markwilliammcdonald@gmail.com>
2020-05-22 15:19:46 -0500

    Added in acceptance criteria to the js file

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+// Starting with the requirements 
+
+/* ACCEPTANCE CRITERIA
+
+
+    WHEN I open the planner
+    THEN the current day is displayed at the top of the calendar
+    --Ideas on what to do--
+    
+
+    WHEN I scroll down
+    THEN I am presented with timeblocks for standard business hours
+    --Ideas on what to do--
+
+
+    WHEN I view the timeblocks for that day
+    THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+    --Ideas on what to do--
+
+
+    WHEN I click into a timeblock
+    THEN I can enter an event
+    --Ideas on what to do--
+
+
+    WHEN I click the save button for that timeblock
+    THEN the text for that event is saved in local storage
+    --Ideas on what to do--
+
+
+    WHEN I refresh the page
+    THEN the saved events persist
+    --Ideas on what to do--
+
+*/
